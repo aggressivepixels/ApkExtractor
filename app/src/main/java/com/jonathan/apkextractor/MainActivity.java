34128e20f2849e08d19c494c86588ed7a1a042ca
Jonathan Hernández <jon210200b@gmail.com>
2016-01-22 13:04:04 -0430

    Removed check for the "action_settings" action bar item. (Forgot to remove it)

--- app/src/main/java/com/jonathan/apkextractor/MainActivity.java
+++ app/src/main/java/com/jonathan/apkextractor/MainActivity.java
@@ -99,10 +99,10 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
-        } else if (id == R.id.action_show_system_apps) {
+        } else*/ if (id == R.id.action_show_system_apps) {
             item.setChecked(!item.isChecked());
             boolean showSystemApps = PreferencesUtils.showSystemApps(this);
             if (showSystemApps != item.isChecked()) {
